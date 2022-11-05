680ce82c6e58f60976ddeb1fff661965bac65313
sturmfx <ttunikitashtobert@gmail.com>
2022-11-05 13:01:08 +0200

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,12 @@
+import java.util.Random;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        Random rand = new Random();
+        Turtle t = new Turtle();
+        t.setCanvasSize(800,800);
+        t.speed(1000);
+    }
+}
